b88cdac8ab64fa72733bcc5cf7592b4814e444fa
le09idas <nicholaspetriello@gmail.com>
2025-01-21 22:18:31 -0500

    Programmed the player object to rotate via A (left) and D (right) button presses.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
+from player import *
 from constants import *
+\
 
 def main():
 
@@ -11,14 +14,20 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
