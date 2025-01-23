96065078999a954f0af97fa1b708c5c1bcdb9c6a
le09idas <nicholaspetriello@gmail.com>
2025-01-23 00:31:06 -0500

    Added asteroids and astroid field; currently the player can move amongst the asteroids but there is no collision or shooting possible.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 # throughout this file
 import pygame
 
-from player import *
 from constants import *
-\
+from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
 
@@ -16,13 +18,23 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
-        player1.update(dt)
-        player1.draw(screen)
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
